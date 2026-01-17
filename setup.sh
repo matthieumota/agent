a3990d5d57fa9f9546d3f9ea8ceb8f8bcc675541
Matthieu Mota <matthieumota@gmail.com>
2026-01-18 00:12:13 +0100

    improve script

--- setup.sh
+++ setup.sh
@@ -5,24 +5,29 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # Create agent user
-sudo useradd -m -s /bin/bash ralph
+if ! id ralph &> /dev/null; then
+    sudo useradd -m -s /bin/bash ralph
+    echo "ralph ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ralph
+fi
+
 sudo mkdir -p /home/ralph/.ssh
-sudo chmod 700 /home/ralph/.ssh
 
 sudo cp /home/ubuntu/.ssh/authorized_keys /home/ralph/.ssh/
 sudo cp /home/ubuntu/.ssh/id_ed25519 /home/ralph/.ssh/
 sudo cp /home/ubuntu/.ssh/app-private-key.pem /home/ralph/.ssh/
+
+sudo chmod 700 /home/ralph/.ssh
 sudo chmod 600 /home/ralph/.ssh/authorized_keys
 sudo chmod 600 /home/ralph/.ssh/id_ed25519
 sudo chmod 600 /home/ralph/.ssh/app-private-key.pem
 sudo chown -R ralph:ralph /home/ralph/.ssh
 
-echo "ralph ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ralph
-
 TOKEN=$(bash ./get-token.sh)
 export TOKEN
 
 sudo -E -u ralph -H bash << 'EOF'
+REPO_URL="github.com/matthieumota/agent.git"
+REPO_DIR="$HOME/agent"
 
 # Move to home directory
 cd $HOME
@@ -38,15 +43,19 @@ sudo apt-get install -y gh libatomic1
 # Install Docker
 if ! command -v docker &> /dev/null; then
     curl -sSL https://get.docker.com | sh
+    sudo usermod -aG docker ralph
 fi
-
-sudo usermod -aG docker ralph
 
 # Install Clawd Bot
 # curl -fsSL https://clawd.bot/install.sh | bash
 
 # Clone the repository
-git clone https://x-access-token:$TOKEN@github.com/matthieumota/agent.git
+if [ ! -d "$REPO_DIR" ]; then
+    git clone https://x-access-token:$TOKEN@${REPO_URL} $REPO_DIR
+else
+    cd $REPO_DIR
+    git pull https://x-access-token:${TOKEN}@${REPO_URL}
+fi
 
 # Configure Git
 git config --global user.name "ralph-vps[bot]"
